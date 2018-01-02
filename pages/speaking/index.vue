baefb240736e46d4b3f9750dc54f3ec9b3f5d093
claudina sarahe <cs@misscs.me>
2018-01-01 23:49:48 -0400

    Speaking section index and view templates

---
+++ pages/speaking/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>Speaking</h1>
+
+      {{ talks.body }}
+
+     <ul>
+       <li v-for="talk in talks.items">
+         {{ talk }}
+      </li>
+     </ul>
+    <nuxt-child />
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData ({ app }) {
+    return {
+      talks: await app.$content('speaking').getAll()
+    }
+  }
+}
+</script>

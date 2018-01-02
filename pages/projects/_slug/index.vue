1ee717e88babea0362f2daba256380623c197029
claudina sarahe <cs@misscs.me>
2018-01-02 10:05:00 -0400

    Putting together project page template

--- pages/projects/_slug/index.vue
+++ pages/projects/_slug/index.vue
@@ -1,9 +1,23 @@
 
 <template>
-  <section class="container bg-near-black vh-100">
-     <h1>Project: {{ project.name }}</h1>
-    <nuxtent-body :body="project.body" />
-  </section>
+  <article class="container white pv6 ph5"
+    v-bind:class="project.background"
+  >
+    <header class="bb bw4 pb3 mb4">
+      <h1 class="f-headline lh-solid mb0 mt0">{{ project.name }}</h1>
+      <h2 class="f1 lh-solid fw8 mb0 mt0">{{ project.description }}</h2>
+      <dl class="lh-solid">
+        <dt class="clip">Date</dt><dd class="dib f1 mr2 fw8">{{ project.date }}</dd>
+        <dt class="clip">Location</dt><dd class="dib f1 fw8 washed-red">{{ project.location }}</dd>
+        </dl>
+    </header>
+    <nuxtent-body :body="project.body" class="lh-copy f5 cc-3 cf-balance mb6" />
+    <div class="mt5 mb6">  
+      <img :src="project.photos" class="br-100 h5 w5 mr6">
+      <img :src="project.photos" class="br-100 h5 w5 mr6">
+      <img :src="project.photos" class="br-100 h5 w5">
+    </div>
+  </article>
 </template>
 
 <script>

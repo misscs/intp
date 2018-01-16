4642c00bb66ee97dc6e1f78e0bfc7591a96af76f
claudina sarahe <cs@misscs.me>
2018-01-16 10:18:05 -0400

    Change date property name

--- pages/projects/_slug/index.vue
+++ pages/projects/_slug/index.vue
@@ -4,19 +4,21 @@
     v-bind:class="project.background"
   >
     <header class="bb bw4 pb3 mb4">
-      <h1 class="f-headline lh-solid mb0 mt0">{{ project.name }}</h1>
-      <h2 class="f1 lh-solid fw8 mb0 mt0">{{ project.description }}</h2>
-      <dl class="lh-solid">
-        <dt class="clip">Date</dt><dd class="dib f1 mr2 fw8">{{ project.date }}</dd>
-        <dt class="clip">Location</dt><dd class="dib f1 fw8 washed-red">{{ project.location }}</dd>
+      <h1 class="f-headline lh-solid mb0 mt0 tracked-tight">{{ project.name }}</h1>
+      <h2 class="f1 lh-solid fw4 mb2 mt0">{{ project.description }}</h2>
+      <dl class="lh-solid fw6">
+        <dt class="clip">Date</dt><dd class="dib f1 mr2 br bw2 pr2">{{ project.timeline }}</dd>
+        <dt class="clip">Location</dt><dd class="dib f1">{{ project.location }}</dd>
         </dl>
     </header>
-    <nuxtent-body :body="project.body" class="lh-copy f5 cc-3 cf-balance mb6" />
-    <div class="mt5 mb6">  
+    <nuxtent-body :body="project.body" class="lh-copy f4 cc-1 measure-narrow cf-balance mb6 nested-copy-line-height" />
+    <p class="fw8">{{ project.role }}</p>
+    <!-- <div class="mt5 mb6">  
       <img :src="project.photos" class="br-100 h5 w5 mr6">
       <img :src="project.photos" class="br-100 h5 w5 mr6">
       <img :src="project.photos" class="br-100 h5 w5">
-    </div>
+    </div> -->
+
   </article>
 </template>
 
